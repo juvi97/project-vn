b24b1cca22d8c0b480e874721687c5ad8ee9283c
jtenner <tenner.joshua@gmail.com>
2014-12-31 13:33:16 -0500

    Add webpack minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
+var webpack = require('webpack');
 module.exports = {
   entry: './index.js',
   output: {
     path: __dirname + "/bundle",
     filename: 'bundle.js',
     publicPath: "/bundle/"
-  }
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
 };
